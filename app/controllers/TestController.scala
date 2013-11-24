1bdeaeb185e8e8c1562e09fac84720b1b7562075
Sandy Maguire <sandy.g.maguire@gmail.com>
2013-11-23 19:23:29 -0500

    Commented up some of the crazier bits

--- app/controllers/TestController.scala
+++ app/controllers/TestController.scala
@@ -8,8 +8,10 @@ import play.api.data.Forms._
 import models._
 
 object TestController extends Controller {
+    // We will parse our form data into this object
     case class WhoIsYourDaddy(daddyName: String)
 
+    // This is the mapping between the form data and our object
     val userForm = Form(mapping(
         "daddyName" -> text
     )(WhoIsYourDaddy.apply)(WhoIsYourDaddy.unapply))
@@ -19,8 +21,13 @@ object TestController extends Controller {
     }
 
     def update = Action { implicit request =>
+        // parse the form data
         val daddy = userForm.bindFromRequest.get
+        
+        // update your daddy
         ExampleModel.whoIsYourDaddy = daddy.daddyName
+        
+        // redirect to home page
         Redirect("/")
     }
 
