f5252776a3266f3a601112c6078721ded50b6d2d
Austin <austin@Gibbon.(none)>
2014-05-11 19:55:43 -0400

    Created some angular functions, got delete and create working for tasks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,8 +4,30 @@ accioApp.controller('TaskCtrl', function($scope, $http) {
 
 
 	$scope.tasks = ["eat", "sleep", "rave", "repeat"];
+	$scope.task = {title : ""};
 
-	$http.get('api/tasks').success(function(data) {
-		$scope.tasks = data;
-	});
+	$scope.update = function() {
+		$http.get('api/tasks').success(function(data) {
+			console.log(data);
+			$scope.tasks = data;
+		});
+	}
+
+
+	$scope.addTask = function() {
+		//This will be the real post parameter
+		//"{title : 'hello world'}"
+		$http.post('api/tasks', $scope.task).success(function(data) {
+			console.log(data);
+			$scope.update();
+		});
+	}
+
+	$scope.deleteTask = function(id) {
+		$http.delete('api/tasks/'+id).success(function() {
+			$scope.update();
+		});
+	}
+
+	$scope.update();
 });
