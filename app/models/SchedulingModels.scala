4a9da1ce333f852af7802ed3f8a3f246bc7d514e
Sandy Maguire <sandy@sandymaguire.me>
2014-05-24 16:48:55 -0400

    Comments and better type evaluation

--- app/models/SchedulingModels.scala
+++ app/models/SchedulingModels.scala
@@ -4,6 +4,7 @@ package models.scheduling
 import scala.collection.immutable._
 import com.github.nscala_time.time.Imports._
 
+// Something we want to accomplish
 case class Task(
   name: String, 
   dueDate: DateTime, 
@@ -11,43 +12,60 @@ case class Task(
   importance: Int
 )
 
+// Scheduled time for Getting Shit Done
 case class WorkChunk(when: DateTime, duration: Duration)
 
+// Helper functions for dealing with lists of WorkChunks
 class WorkChunkList(chunks: Seq[WorkChunk]) {
-  def spendableTime: Duration = new Duration(
-    chunks.foldLeft(0: Long)(
-      (time, chunk) => time + chunk.duration.millis
-    )
+  // Seq[WorkChunk] => Duration
+  def spendableTime = chunks.foldLeft[Duration](0 seconds)(
+    (time, chunk) => time + chunk.duration
   )
 }
+
+// A scheduled event
+case class Event(task: Task, when: DateTime, duration: Duration)
 
 
 class Scheduler(allTasks: Seq[Task], workChunks: Seq[WorkChunk]) {
   // Allows us to call WorkChunkList methods on work chunk lists
   implicit def listWrapper(chunks: Seq[WorkChunk]) = new WorkChunkList(chunks)
 
+  // Parameters for tweaking the scheduling
+  object params {
+    // Need to have at least this much time free at any given point in the
+    // scheduling algo (ensure we have time in case of emergencies)
+    val minBufferTime = 1 hours
+
+    // Only schedule this much time per project per day (prevent burnouts)
+    val maxProjectTimePerSession = 4 hours
+  }
+
 
   val tasks = allTasks.sortBy { task => task.dueDate }
 
-  object params {
-    val minimumBufferTime = 1 hours
-  }
-
-  def getSpendableTimeDuring(start: DateTime, end: DateTime) = 
+  // Gets the duration of all workchunks between two dates
+  def getSpendableTimeDuring(start: DateTime, end: DateTime): Duration = 
     workChunks.filter(
       chunk => start <= chunk.when && chunk.when < end
     ).spendableTime
 
+
+  // Determine if we have enough time in our workchunks to meet all of our
+  // deadlines
   def hasEnoughTime: Boolean = {
     var curScheduleStart = DateTime.now
+
+    // Measures the amount of time in our workchunks that is not scheduled by
+    // the time we've met this deadline
     var timeToSpend: Duration = 0.seconds
 
     for (task <- tasks) {
-      timeToSpend += 
-        getSpendableTimeDuring(curScheduleStart, task.dueDate) - 
-          task.duration
+      timeToSpend += getSpendableTimeDuring(curScheduleStart, task.dueDate) - 
+        task.duration
 
-      if (timeToSpend <= params.minimumBufferTime) {
+      if (timeToSpend <= params.minBufferTime) {
+        // Fail if we don't have enough of a time buffer to meet this deadline
         return false
       }
 
@@ -55,6 +73,11 @@ class Scheduler(allTasks: Seq[Task], workChunks: Seq[WorkChunk]) {
     }
 
     true
+  }
+
+  // Schedule events into workchunks
+  def doScheduling: Seq[Event] = {
+    List()
   }
 }
 
