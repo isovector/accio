d17c054241e27e4e7768755da09479caa286641a
Austin <duricooldude@gmail.com>
2014-05-24 23:33:39 -0400

    cleaning up some pull request stuff

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -78,37 +78,6 @@ accioApp.controller('TaskCtrl', ['$scope', '$http', 'TaskService', function($sco
 	$scope.update();
 }]);
 
-accioApp.controller('CalendarCtrl', ['$scope', '$http', function($scope, $http) {
-
-	$scope.tasks = [];
-	$scope.task = {title : "calnder"};
-
-	$scope.test = function(){
-		alert("jj");
-	}
-
-	
-	scheduler.init('scheduler_here', new Date(), "month");
-
-	//$scope.scheduler = scheduler;
-}]);
-
-accioApp.directive('calendar', function() {
-	return {
-		//Only apply this directive to elements with this name
-		restrict: 'A',
-		//replace the element with the template
-		replace: true,
-		templateUrl: "/assets/directives/calendar.partial.html",
-		link: function(scope, element, attributes) {
-			scheduler.init('scheduler_here', new Date(), "month");
-		// on button click get events from server
-		// and then parse them to calendar		
-		}
-	}
-	
-});
-
 accioApp.directive('categoryList', function() {
 	return {
 		//Only apply this directive to elements with this name
